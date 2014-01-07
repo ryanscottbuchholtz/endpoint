573ad77d13280502c2ddef29948fddb907190da0
Debora Blass <debbieblass@gmail.com>
2014-01-07 14:49:36 -0500

    added simple form

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,22 @@
+class ContactsController < ApplicationController
+
+  def new
+      @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(set_params)
+
+    if @contact.save
+        redirect_to root_path, notice:"Success"
+    else
+        render :new
+    end  
+  end
+
+  private
+
+  def set_params
+    params.require(:contact).permit(:first_name, :last_name, :email, :company)
+  end
+end
